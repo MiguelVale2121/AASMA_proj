6d996ba952049e194a599aae77c1b590ccd8d2fa
Miguel Vale <msvv2014@gmail.com>
2024-05-14 19:15:02 +0100

    refactoring

--- randomAgent.py
+++ randomAgent.py
@@ -4,12 +4,15 @@ class RandomAgent:
     def __init__(self, role='prey'):
         self.role = role
         self.moves = {
-            'prey': [('up', 0, -1), ('down', 0, 1), ('left', -1, 0), ('right', 1, 0)],
+            'prey1': [('up', 0, -1), ('down', 0, 1), ('left', -1, 0), ('right', 1, 0)],
+            'prey2': [('up', 0, -1), ('down', 0, 1), ('left', -1, 0), ('right', 1, 0)],
             'hunter': [('up', 0, -1), ('down', 0, 1), ('left', -1, 0), ('right', 1, 0)]
         }
     
     def choose_action(self, state):
-        if self.role == 'prey':
-            return random.choice(self.moves['prey'])
+        if self.role == 'prey1':
+            return random.choice(self.moves['prey1'])
+        elif self.role == 'prey2':
+            return random.choice(self.moves['prey2'])
         elif self.role == 'hunter':
             return random.choice(self.moves['hunter'])
