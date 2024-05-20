805de8217db2c4c101623dbf1cb2c4ba53483398
Miguel Vale <msvv2014@gmail.com>
2024-05-20 16:16:23 +0100

    feat: creation of the hunterAgent

---
+++ hunterAgent.py
@@ -0,0 +1,41 @@
+
+class HunterAgent:
+    def __init__(self):
+        pass
+
+    def choose_action(self, state):
+        hunter_pos = state['hunter_pos']
+        prey1_pos = state['prey1_pos']
+        prey2_pos = state['prey2_pos']
+        obstacles = state['obstacles']
+        
+        # Determine which prey to chase (prefer prey1 if both are active)
+        target_prey_pos = prey1_pos if state['prey1_active'] else prey2_pos
+        
+        # If both preys are inactive, no movement is necessary
+        if target_prey_pos is None:
+            return None, None, None
+
+        # Calculate possible moves
+        possible_moves = ['up', 'down', 'left', 'right']
+        best_move = None
+        min_distance = float('inf')
+
+        for move in possible_moves:
+            new_position = hunter_pos[:]
+            if move == 'up' and hunter_pos[1] > 0:
+                new_position[1] -= 1
+            elif move == 'down' and hunter_pos[1] < state['grid_size'] - 1:
+                new_position[1] += 1
+            elif move == 'left' and hunter_pos[0] > 0:
+                new_position[0] -= 1
+            elif move == 'right' and hunter_pos[0] < state['grid_size'] - 1:
+                new_position[0] += 1
+            
+            if tuple(new_position) not in obstacles:
+                distance = abs(new_position[0] - target_prey_pos[0]) + abs(new_position[1] - target_prey_pos[1])
+                if distance < min_distance:
+                    min_distance = distance
+                    best_move = move
+        
+        return best_move, None, None
